9db2b5c739be43e29e6669aeb8e316e15e68af21
saiteja <tsaiteja7@gmail.com>
2020-05-14 04:08:54 +0530

    added candidates list

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,26 @@ pragma solidity ^0.5.16;
 
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
 
-    // Constructor
-    function Elections() public {
-        candidate = "Candidate 1";
+    // Read/write candidates
+    mapping(uint256 => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint256 public candidatesCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
